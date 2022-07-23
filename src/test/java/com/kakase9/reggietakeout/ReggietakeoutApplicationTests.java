0c1b9913f791be2b857bcab1adb2820bc691c21e
create <2302704370@qq.com>
2022-07-23 12:36:01 +0800

    work final

--- src/test/java/com/kakase9/reggietakeout/ReggietakeoutApplicationTests.java
+++ src/test/java/com/kakase9/reggietakeout/ReggietakeoutApplicationTests.java
@@ -1,13 +1,32 @@
 package com.kakase9.reggietakeout;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-class ReggietakeoutApplicationTests {
+import javax.annotation.security.RunAs;
 
-    @Test
-    void contextLoads() {
-    }
+//@SpringBootTest
+//@RunWith(SpringRunner.class)
+//public class ReggietakeoutApplicationTests {
 
-}
+//    @Autowired
+//    private RedisTemplate redisTemplate;
+//
+//    @Test
+//    public void testString(){
+//        ValueOperations valueOperations = redisTemplate.opsForValue();//简单k-v操作
+//        valueOperations.set("city","hp");
+//    }
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+
+
+//}
